46355e24551a10e88619f39fa4b2f46a47a814c4
Martin Zvozil <martin.zvozil@gmail.com>
2020-08-10 15:05:20 +0200

    Updates

---
+++ precache-manifest.f6edb7c15691bedbefba64f0c96ae463.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66dd104464220a21c3eeb11ed87486d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09082a45acc857abb491",
+    "url": "/static/css/main.6f3dc0bf.chunk.css"
+  },
+  {
+    "revision": "2bb877a4b45951ef2aba",
+    "url": "/static/js/2.c8a631bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c8a631bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09082a45acc857abb491",
+    "url": "/static/js/main.2d85006a.chunk.js"
+  },
+  {
+    "revision": "6c5c0543792299806635",
+    "url": "/static/js/runtime-main.1d2d4193.js"
+  },
+  {
+    "revision": "46c90052450754eb6dffe8647fd48dc9",
+    "url": "/static/media/blackBlackInterior.46c90052.png"
+  },
+  {
+    "revision": "9d717c7284ff57c4b8143353395e0b4b",
+    "url": "/static/media/blackSilverInterior.9d717c72.png"
+  },
+  {
+    "revision": "6202eff806ec2c516499a5ddcd7d9c39",
+    "url": "/static/media/bmw3_black.6202eff8.png"
+  },
+  {
+    "revision": "e0678124740d00cbf2eec93d4580a0c6",
+    "url": "/static/media/bmw3_black_premium_wheels.e0678124.png"
+  },
+  {
+    "revision": "e1a6987c8734416b07f3b9be6184a0c0",
+    "url": "/static/media/bmw3_blue.e1a6987c.png"
+  },
+  {
+    "revision": "5b3208d421e06cd5ba9287f0aacb69a6",
+    "url": "/static/media/bmw3_blue_premium_wheels.5b3208d4.png"
+  },
+  {
+    "revision": "01c8b2fcba7b2db57a13142b208aaa7e",
+    "url": "/static/media/bmw3_orange.01c8b2fc.png"
+  },
+  {
+    "revision": "d35d793b1127b739e0024da537b251be",
+    "url": "/static/media/bmw3_orange_premium_wheels.d35d793b.png"
+  },
+  {
+    "revision": "d938175b250c030fd2770a5a0844dc47",
+    "url": "/static/media/bmw3_white.d938175b.png"
+  },
+  {
+    "revision": "011284808d2c38a14f20e7c289865fb0",
+    "url": "/static/media/bmw3_white_premium_wheels.01128480.png"
+  },
+  {
+    "revision": "0e07e5445df7aa50d5f3d5e57f9d0eb4",
+    "url": "/static/media/lightBlackInterior.0e07e544.png"
+  },
+  {
+    "revision": "82e1e15493e87a6bcf94fd9decd55dbf",
+    "url": "/static/media/lightSilverInterior.82e1e154.png"
+  }
+]);
